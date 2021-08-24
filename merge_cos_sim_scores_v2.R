2ce9ba04c512ebd1eff8da625d1ae61b4d0e31e9
Ande Gao <aargue19@gmail.com>
2021-08-25 06:16:32 +0800

    changed all.y to all.x in merge_cos_sims

--- merge_cos_sim_scores_v2.R
+++ merge_cos_sim_scores_v2.R
@@ -1,4 +1,6 @@
 # USE THIS TO CALCULATE ALL COS SIMS BASED ON PROTOTYPES USING 1 AND 2 YEARS
+df = read.csv("C:/Users/gaoan/Desktop/step8/merged_data_w_dummies.csv")
+
 setwd("C:/Users/gaoan/Desktop/step8/cos_sim")
 
 ids_no_2005_2006 = unique(df$id[df$release_year != "2005" & df$release_year != "2006"])
@@ -17,7 +19,7 @@ for (fil in list.files("C:/Users/gaoan/Desktop/step8/cos_sim")){
   
   colnames(current_df) = c("id", cname1yr, cname2yr)
   
-  big_df = merge(big_df, current_df, by= "id", all.y = T)
+  big_df = merge(big_df, current_df, by= "id", all.x = T)
   print(ncol(big_df))
 }
 
